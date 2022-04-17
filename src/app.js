9edbee4294f387ed07729001a7d2cc46e7a0e402
Michelle Pruitt <86678965+pruittme1@users.noreply.github.com>
2022-04-17 19:10:11 -0400

    adding forecast

--- src/app.js
+++ src/app.js
@@ -22,10 +22,15 @@ function formateDate(timestamp) {
 }
 
 function displayForecast() {
+  console.log(response.data.daily);
+}
+
+let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+
+function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
@@ -54,6 +59,10 @@ function displayForecast() {
 
 function getForecast(coordinates) {
   console.log(coordinates);
+  let apiKey = "87b178ac3ff18ae30998e0fe85f9d0ad";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
