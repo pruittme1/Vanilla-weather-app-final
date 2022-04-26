678a569a4e624c746a6b009505c7f6676134a891
Michelle Pruitt <86678965+pruittme1@users.noreply.github.com>
2022-04-25 22:29:03 -0400

    added weekly forecast js

--- src/app.js
+++ src/app.js
@@ -21,6 +21,14 @@ function formateDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formateDate(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
