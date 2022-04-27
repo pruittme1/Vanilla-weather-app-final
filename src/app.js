e4096a071ac7211477b5f2c862791ef25fd1cc89
Michelle Pruitt <86678965+pruittme1@users.noreply.github.com>
2022-04-26 23:08:18 -0400

    cleaning up code

--- src/app.js
+++ src/app.js
@@ -68,19 +68,15 @@ function displayForecast(response) {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "87b178ac3ff18ae30998e0fe85f9d0ad";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
